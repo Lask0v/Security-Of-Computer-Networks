c080c6c6e5d01716f10931842d23f62cd34e747b
olssssi <64044694+olssssi@users.noreply.github.com>
2022-03-22 19:24:19 +0100

    Decyrption of shift matrix

---
+++ src/main/java/pl/wipb/securityofcomputernetworks/algorithms/shiftmatrix/ShiftMatrix.java
@@ -0,0 +1,110 @@
+package pl.wipb.securityofcomputernetworks.algorithms.shiftmatrix;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+@RestController
+@RequestMapping("/shift-matrix")
+public class ShiftMatrix {
+
+    @GetMapping("/encrypt")
+    public String encrypt(@RequestParam String message, @RequestParam String key) {
+        int[] sequence = Arrays.stream(key.trim().split("-")).mapToInt(Integer::parseInt).toArray();
+        HashMap<Integer, LinkedList<Character>> map = new HashMap<>();
+
+        int temp_counter = 0;
+        int counter = 0;
+
+        for (int i = 0; i < message.length(); i++) {
+            map.putIfAbsent(counter, new LinkedList<>());
+            if (temp_counter++ >= sequence.length - 1) {
+                counter++;
+                temp_counter = 0;
+            }
+        }
+
+        counter = 0;
+        temp_counter = 0;
+        for (int i = 0; i < message.length(); i++) {
+            map.get(counter).add(message.charAt(i));
+            if (temp_counter++ >= sequence.length - 1) {
+                ++counter;
+                temp_counter = 0;
+            }
+        }
+
+        int rowSize = sequence.length;
+        int lastRowSize = map.get(sequence.length-1).size();
+
+        if(lastRowSize < rowSize){
+            // wypełnienie gwiazdkami pustych miejsc w ostatnim wierszu
+            for(int j=lastRowSize; j<rowSize; j++){
+                map.get(counter).add('*');
+            }
+        }
+
+
+        StringBuilder cipheredText = new StringBuilder();
+
+        map.keySet().forEach(keyMap -> {
+            for (int sequenceKey : sequence) {
+                if (map.get(keyMap).size() >= sequenceKey)
+                    cipheredText.append(map.get(keyMap).get(sequenceKey - 1));
+            }
+        });
+
+        return cipheredText.toString();
+    }
+
+    @GetMapping("/decrypt")
+    public String decrypt(@RequestParam String message, @RequestParam String key) {
+        int[] sequence = Arrays.stream(key.trim().split("-")).mapToInt(Integer::parseInt).toArray();
+        int[] revertedSequence = new int[sequence.length];
+        HashMap<Integer, LinkedList<Character>> map = new HashMap<>();
+
+        // odwracanie klucza
+        for (int i=0; i<sequence.length; i++){
+            revertedSequence[i] = sequence[sequence.length-i-1];
+        }
+
+        int temp_counter = 0;
+        int counter = 0;
+
+        for (int i = 0; i < message.length(); i++) {
+            map.putIfAbsent(counter, new LinkedList<>());
+            if (temp_counter++ >= revertedSequence.length - 1) {
+                counter++;
+                temp_counter = 0;
+            }
+        }
+
+        counter = 0;
+        temp_counter = 0;
+        for (int i = 0; i < message.length(); i++) {
+            map.get(counter).add(message.charAt(i));
+            if (temp_counter++ >= revertedSequence.length - 1) {
+                ++counter;
+                temp_counter = 0;
+            }
+        }
+
+        // odszyfrowywanie tekstu
+        StringBuilder cipheredText = new StringBuilder();
+        map.keySet().forEach(keyMap -> {
+            for (int sequenceKey : revertedSequence) {
+                if (map.get(keyMap).size() >= sequenceKey)
+                    if(!map.get(keyMap).get(sequenceKey-1).equals('*')){    // pominięcie znaków gwiazdki, które służyły przy szyfrowaniu jako wypełnienie niepełnego wiersza
+                        cipheredText.append(map.get(keyMap).get(sequenceKey - 1));
+                    }
+            }
+        });
+
+        return cipheredText.toString();
+    }
+}
---
+++ src/test/java/pl/wipb/securityofcomputernetworks/ShiftMatrixTest.java
@@ -0,0 +1,71 @@
+package pl.wipb.securityofcomputernetworks;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.wipb.securityofcomputernetworks.algorithms.shiftmatrix.ShiftMatrix;
+
+@SpringBootTest
+class ShiftMatrixTest {
+
+    @Autowired
+    ShiftMatrix shiftMatrix;
+
+    // dl.wiadomosci%dl.klucza=3
+    @Test
+    void test_encrypt_3() {
+        String messageToEncrypt = shiftMatrix.encrypt("CRYPTOGRAPHYOSA","3-1-4-2");
+        Assertions.assertEquals("YCPRGTROHAYPAO*S", messageToEncrypt);
+    }
+
+    // dl.wiadomosci%dl.klucza=2
+    @Test
+    void test_encrypt_2() {
+        String messageToEncrypt = shiftMatrix.encrypt("CRYPTOGRAPHYSA","3-1-4-2");
+        Assertions.assertEquals("YCPRGTROHAYP*S*A", messageToEncrypt);
+    }
+
+    // dl.wiadomosci%dl.klucza=1
+    @Test
+    void test_encrypt_1() {
+        String messageToEncrypt = shiftMatrix.encrypt("CRYPTOGRAPHYS","3-1-4-2");
+        Assertions.assertEquals("YCPRGTROHAYP*S**", messageToEncrypt);
+    }
+
+    // dl.wiadomosci%dl.klucza=0
+    @Test
+    void test_encrypt_0() {
+        String messageToEncrypt = shiftMatrix.encrypt("CRYPTOGRAPHYOSALK","3-1-4-2");
+        Assertions.assertEquals("YCPRGTROHAYPAOLSK", messageToEncrypt);
+    }
+
+    // dl.wiadomosci%dl.klucza=3
+    @Test
+    void test_decrypt_3() {
+        String messageToDecrypt = shiftMatrix.decrypt("YCPRGTROHAYPAO*S","3-1-4-2");
+        Assertions.assertEquals("CRYPTOGRAPHYOSA", messageToDecrypt);
+    }
+
+    // dl.wiadomosci%dl.klucza=2
+    @Test
+    void test_decrypt_2() {
+        String messageToDecrypt = shiftMatrix.decrypt("YCPRGTROHAYP*S*A","3-1-4-2");
+        Assertions.assertEquals("CRYPTOGRAPHYSA", messageToDecrypt);
+    }
+
+    // dl.wiadomosci%dl.klucza=1
+    @Test
+    void test_decrypt_1() {
+        String messageToDecrypt = shiftMatrix.decrypt("YCPRGTROHAYP*S**","3-1-4-2");
+        Assertions.assertEquals("CRYPTOGRAPHYS", messageToDecrypt);
+    }
+
+    // dl.wiadomosci%dl.klucza=0
+    @Test
+    void test_decrypt_0() {
+        String messageToDecrypt = shiftMatrix.decrypt("YCPRGTROHAYPAOLSK","3-1-4-2");
+        Assertions.assertEquals("CRYPTOGRAPHYOSALK", messageToDecrypt);
+    }
+
+}
