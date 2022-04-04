f410a7babd73614df506d43dd552ee3fb8b9148f
olssssi <64044694+olssssi@users.noreply.github.com>
2022-04-04 11:31:35 +0200

    LSFR implementation

--- src/main/java/pl/wipb/securityofcomputernetworks/algorithms/generator/Generator.java
+++ src/main/java/pl/wipb/securityofcomputernetworks/algorithms/generator/Generator.java
@@ -14,10 +14,11 @@ import java.util.List;
 public class Generator {
 
     @GetMapping()
+    // Wielomian przyjmujemy w postaci stringa np. "1 + 2x2 + 3x4 + x6"
     public String encrypt(@RequestParam String polynomial) {
         List<PolynomialComponent> polynomialComponentList = new ArrayList<>();
 
-//       TODO: obsłużyć przypadki 1) np. "1" bez "x^0" oraz 2) np. "x4" bez współczynnika przed x
+//       TODO: obsłużyć przypadki 1) np. "1" bez "x0" oraz 2) np. "x4" bez współczynnika przed x
         List<String> list = List.of(polynomial.split(" + "));
         int counter = 0;
         for (String s : list) {
@@ -35,6 +36,62 @@ public class Generator {
         }
 
         return null;
+    }
+
+    @GetMapping("/generator")
+    public void gen(){
+        //Wynik -pozniej to mozna przerobic zeby nie bylo na liscie ale nwm
+        List<Integer> result = new ArrayList<>();
+
+        //Na sztywno podany wielomian x^1+2x^2+3x^4+x^6
+        List<PolynomialComponent> polynomialComponentList = new ArrayList<>();
+        polynomialComponentList.add(new PolynomialComponent(1,1));
+        polynomialComponentList.add(new PolynomialComponent(2,2));
+        polynomialComponentList.add(new PolynomialComponent(3,4));
+        polynomialComponentList.add(new PolynomialComponent(1,6));
+
+        //Ziarno. Podane birnarnie (na razie na sztywno) ->
+        //TODO trzeba zrobic na to walidacje
+        //TODO walidacje na to, czy jego dlugosc == najwyzsza potega wielomianu
+        List<Integer> seed = new ArrayList<>();
+        seed.add(0);
+        seed.add(1);
+        seed.add(0);
+        seed.add(1);
+        seed.add(0);
+        seed.add(1);
+
+        //TODO: to ma byc w petli
+        //Pobranie wartości do xora
+        List<Integer> xorList = new ArrayList<>();
+        for (PolynomialComponent component:polynomialComponentList) {
+            if(component.degree!=0){
+                xorList.add(seed.get(component.degree-1));
+            }
+        }
+
+        //Przesuniecie
+        //Dodanie na poczatek seeda wyniku xora
+        seed.add(0,executeXor(xorList));
+        //Dodanie do wyniku ostatniej wartości xora
+        result.add(seed.get(seed.size()-1));
+        //Usuniecie z seeda ostatniej wartosci
+        seed.remove(seed.size()-1);
+
+        //TODO: tu koniec petli
+
+
+
+
+
+    }
+
+    // Funkcja xorująca (jeżeli cokolwiek jest 1 -> wypisz 1; w przeciwnym wypadku wypisz 0)
+    public int executeXor(List<Integer> xorList){
+       return xorList.stream()
+                .filter(number -> number==1)
+                .findAny()
+                .orElse(0);
     }
 
     // Wyraz wielomianu, czyli np "2x^4"
