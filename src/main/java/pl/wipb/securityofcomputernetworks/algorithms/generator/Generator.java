9850a520322af87d8c1dc3b40f17a52790807755
olssssi <64044694+olssssi@users.noreply.github.com>
2022-04-07 10:00:19 +0200

    LSFR generowanie wyniku już w sposób poprawny. Nadal trzeba zaimplementować wczytywanie wielomianu i pobieranie od użytkownika długości wyniku

--- src/main/java/pl/wipb/securityofcomputernetworks/algorithms/generator/Generator.java
+++ src/main/java/pl/wipb/securityofcomputernetworks/algorithms/generator/Generator.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 @RestController
 @RequestMapping("/generator")
@@ -44,58 +45,60 @@ public class Generator {
         //Wynik -pozniej to mozna przerobic zeby nie bylo na liscie ale nwm
         List<Integer> result = new ArrayList<>();
 
-        //Na sztywno podany wielomian x^1+2x^2+3x^4+x^6
+        //Na sztywno podany wielomian z przykładu
         List<PolynomialComponent> polynomialComponentList = new ArrayList<>();
+        polynomialComponentList.add(new PolynomialComponent(1,0));
         polynomialComponentList.add(new PolynomialComponent(1,1));
-        polynomialComponentList.add(new PolynomialComponent(2,2));
-        polynomialComponentList.add(new PolynomialComponent(3,4));
-        polynomialComponentList.add(new PolynomialComponent(1,6));
+        polynomialComponentList.add(new PolynomialComponent(1,4));
 
-        //Ziarno. Podane birnarnie (na razie na sztywno) ->
+        //Ziarno. Podane birnarnie (na razie na sztywno z przykładu) ->
         //TODO trzeba zrobic na to walidacje
         //TODO walidacje na to, czy jego dlugosc == najwyzsza potega wielomianu
         List<Integer> seed = new ArrayList<>();
-        seed.add(0);
-        seed.add(1);
-        seed.add(0);
-        seed.add(1);
-        seed.add(0);
-        seed.add(1);
 
-        //TODO: to ma byc w petli
-        //Pobranie wartości do xora
-        List<Integer> xorList = new ArrayList<>();
-        for (PolynomialComponent component:polynomialComponentList) {
-            if(component.degree!=0){
-                xorList.add(seed.get(component.degree-1));
+        seed.add(0);
+        seed.add(1);
+        seed.add(1);
+        seed.add(0);
+
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> xorList;
+
+        final int wantedResultElements = 7;
+        for(int i = 0 ; i<wantedResultElements; i++){
+            xorList = new ArrayList<>();
+            for (PolynomialComponent component : polynomialComponentList) {
+                if (component.degree != 0) {
+                    xorList.add(seed.get(component.degree - 1));
+                }
             }
+
+            //Przesuniecie
+            //Dodanie na poczatek seeda wyniku xora
+            seed.add(0, executeXor(xorList));
+            //Dodanie do wyniku ostatniej wartości xora
+            result.add(seed.get(seed.size() - 1));
+            //Usuniecie z seeda ostatniej wartosci
+            seed.remove(seed.size() - 1);
         }
 
-        //Przesuniecie
-        //Dodanie na poczatek seeda wyniku xora
-        seed.add(0,executeXor(xorList));
-        //Dodanie do wyniku ostatniej wartości xora
-        result.add(seed.get(seed.size()-1));
-        //Usuniecie z seeda ostatniej wartosci
-        seed.remove(seed.size()-1);
-
-        //TODO: tu koniec petli
-
-
-
-
-
+        System.out.println(result);
     }
 
-    // Funkcja xorująca (jeżeli cokolwiek jest 1 -> wypisz 1; w przeciwnym wypadku wypisz 0)
+    // Funkcja xorująca (jeżeli xor ma wszystkie wartości=1 lub wszystkie wartości=0, zwróć 0 jako wynik
+    //                  jeżeli xor ma wartości różnej wielkości, zwróć 1 jako wynik)
     public int executeXor(List<Integer> xorList){
-       return xorList.stream()
-                .filter(number -> number==1)
-                .findAny()
-                .orElse(0);
+       int min = xorList.stream()
+               .min(Integer::compare)
+               .get();
+       int max = xorList.stream()
+               .max(Integer::compare)
+               .get();
+
+        return min == max ? 0 : 1;
     }
 
-    // Wyraz wielomianu, czyli np "2x^4"
+    // Wyraz wielomianu, czyli np "2x^4" -> w tym przypadku 2 to coefficient, a 4 to degree
     @AllArgsConstructor
     private class PolynomialComponent {
         Integer coefficient;
